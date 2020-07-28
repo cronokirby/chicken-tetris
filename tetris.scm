c996d304ab4715ac06354bef09d1a726896fa08a
Lúcás Meier <cronokirby@gmail.com>
2020-07-28 14:30:25 +0200

    Add a naive game loop
    The loop just sleeps based on a guestimate, and doesn't do any fancy
    fluidity or lag correction. This should be enough for our purposes.
    
    We now have a bit of state for the game, which is just a counter in
    order to update a color.

--- tetris.scm
+++ tetris.scm
@@ -1,20 +1,31 @@
 (import (prefix sdl2 "sdl2:"))
+(import (chicken time))
 
 ;; Colors
 (define color-blue (sdl2:make-color 0 0 255))
 
+(define (make-color x)
+  (sdl2:make-color 0 x x))
 
 ;; This section defines the logic to render the game view
 
 ;; Render the current state of the game to a window
 ;; Right now there's no state, so we just draw a blue screen on the window
-(define (render! window)
+(define (render! state window)
   (sdl2:fill-rect!
     (sdl2:window-surface window)
     #f
-    color-blue)
+    (make-color state))
   (sdl2:update-window-surface! window))
 
+(define (update state)
+  (remainder (+ state 1) 255))
+
+(define MS-PER-FRAME 16.6)
+
+(define (wait-time then now)
+  (let ((delta (- (+ then MS-PER-FRAME) now)))
+    (if (< delta 0) 0 (floor delta))))
 
 ;; Start running the game in a loop until the user quits
 (define (main)
@@ -25,11 +36,18 @@
   (sdl2:init! '(video))
   (define window (sdl2:create-window! "Hello, World!" 0 0 600 400))
 
+  (define state 0)
+  (define then (current-milliseconds))
+
   ;; Our game loop, which keeps spinning until the user quits, about which SDL2
   ;; will inform us.
   (let loop ()
     (unless (sdl2:quit-requested?)
-      (render! window)
+      (set! state (update state))
+      (render! state window)
+      (let ((now (current-milliseconds)))
+        (sdl2:delay! (wait-time then now))
+        (set! then now))
       (loop))))
 
 (main)
